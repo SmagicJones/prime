fc75b0b50469c30344cdaffacb3dab3dfc4605cf
Smagic Jones <>
2023-03-26 14:33:12 +0100

    A prime number finder

---
+++ prime.js
@@ -0,0 +1,15 @@
+function isPrime(number){
+    if(number < 2){
+        return false;
+    }
+
+    for(let i = 2; i < number; i++){
+        if(number % i === 0){
+            console.log(number % i);
+            return false;
+        }
+    }
+    return true;
+}
+
+isPrime(16);
